a8dcaa0a52550f7b63e98841d9db251e0c0c81d7
Bryan Paxton <starbelly@pobox.com>
2019-03-23 15:21:53 -0500

    Initial commit

---
+++ lib/shorty.ex
@@ -0,0 +1,18 @@
+defmodule Shorty do
+  @moduledoc """
+  Documentation for Shorty.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Shorty.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
